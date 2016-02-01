b33693feff0300da13ff4d5bb944087f7d60ee9f
zllhyq <zllhyq@zllhyq-PC>
2016-02-01 15:30:33 +0800

    2

--- gittest/src/main.java
+++ gittest/src/main.java
@@ -1,4 +1,3 @@
-
 public class main {
 
 	/**
@@ -6,7 +5,7 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("test");
 	}
 
 }
